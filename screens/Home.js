a409b09ef96799f32326542292c7a9cf2a6e5f4f
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-06-24 20:57:23 -0400

    API & Redux

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
 import MainLayout from "./MainLayout";
+import { connect } from "react-redux";
+import { getHoldings, getCoinMarket } from "../stores/market/marketActions";
 
 const Home = () => {
   return (
@@ -12,4 +14,18 @@ const Home = () => {
   );
 };
 
-export default Home;
+// function mapStateToProps(state) {
+//   return {
+//     myHoldings: state.marketReducer.myHoldings,
+//     coins: state.marketReducer.coins,
+//   };
+// }
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     setTradeModalVisibility: (isVisible) => {
+//       return dispatch(setTradeModalVisibility(isVisible));
+//     },
+//   };
+// }
+
+export default connect(Home);
